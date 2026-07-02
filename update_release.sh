192ff27ac42fdb3e7e67046d916b4e344514322e
Isaac Peterson <isaacwilliam4@gmail.com>
2026-07-02 14:53:10 -0600

    update update release script

--- update_release.sh
+++ update_release.sh
@@ -1,10 +1,10 @@
-# check if .env exists if not fail
+# check if .env.alphaz exists if not fail
 
-if [ ! -f .env ]; then
-  echo ".env file not found. Please create a .env file based on .env.example."
+if [ ! -f .env.alphaz ]; then
+  echo ".env.alphaz file not found. Please create a .env.alphaz file based on .env.alphaz.example."
   exit 1
 fi
 
-source .env
+source .env.alphaz
 docker compose -f docker-compose.yml build gadm-eval --no-cache
 docker compose -f docker-compose.yml up gadm-eval
