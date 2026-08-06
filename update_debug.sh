ca9b1bf587980790651ead1ecb419f71352fe4b7
Isaac Peterson <isaacwilliam4@gmail.com>
2026-08-05 21:14:22 -0600

    update debug now updates webuit alos

--- update_debug.sh
+++ update_debug.sh
@@ -8,3 +8,4 @@ fi
 source .env.debug
 docker compose -f docker-compose-debug.yml up -d langfuse-web
 docker compose -f docker-compose-debug.yml up --build -d gadm-eval
+docker compose -f docker-compose-debug.yml up --build -d gadm-webui
